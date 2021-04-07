4cf2e445e9f72f73f2b15a2c77758988a60fe07f
APB-s <a.saillour@protonmail.com>
2021-04-07 16:09:29 +0200

    test

---
+++ app/src/main/java/com/example/projet_cleder_resolution/accueil.java
@@ -0,0 +1,14 @@
+package com.example.projet_cleder_resolution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accueil);
+    }
+}
