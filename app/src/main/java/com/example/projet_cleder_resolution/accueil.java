80b839bb333a21992bf526c8496fb4d5fdfa006f
SGLover28 <lilian.hennechart.ETU@univ-lemans.fr>
2021-04-16 12:00:57 +0200

    Ajout

--- app/src/main/java/com/example/projet_cleder_resolution/accueil.java
+++ app/src/main/java/com/example/projet_cleder_resolution/accueil.java
@@ -1,6 +1,8 @@
 package com.example.projet_cleder_resolution;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,9 +12,9 @@ public class accueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil);
+    }
 
-
-
-
+    public void goToSettings(View view){
+        Intent intent = new Intent(this, Settings.class);
     }
 }
