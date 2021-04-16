f3783d810c37c399de488bb9d8a7c30b00bf197c
SGLover28 <lilian.hennechart.ETU@univ-lemans.fr>
2021-04-16 12:16:32 +0200

    Ajout settings

--- app/src/main/java/com/example/projet_cleder_resolution/accueil.java
+++ app/src/main/java/com/example/projet_cleder_resolution/accueil.java
@@ -3,6 +3,7 @@ package com.example.projet_cleder_resolution;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +13,16 @@ public class accueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil);
+
+        ImageView iv = findViewById(R.id.settingsButton);
+        iv.setOnClickListener(this::goToSettings);
+
+
+
     }
 
     public void goToSettings(View view){
         Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
