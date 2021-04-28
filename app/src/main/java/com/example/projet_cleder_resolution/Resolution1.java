da6f25ba884ae3555cdb0ea5a8d35bc717ed6e92
APB-s <a.saillour@protonmail.com>
2021-04-28 16:06:36 +0200

    Corretion d'un bug d'affichage (à voir!!!! mauvaise dimension) Création de la page modification

--- app/src/main/java/com/example/projet_cleder_resolution/Resolution1.java
+++ app/src/main/java/com/example/projet_cleder_resolution/Resolution1.java
@@ -1,13 +1,28 @@
 package com.example.projet_cleder_resolution;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Resolution1 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resolution1);
+
+        //création d'un bouton (ici bouton modifier)
+        ImageView modifier_button = findViewById(R.id.modifier);
+        modifier_button.setOnClickListener(this::modifier_res);
+    }
+
+    public void modifier_res(View view){
+        Intent intent = new Intent(this, modifier_bonne_resolution.class);
+        startActivity(intent);
     }
 }
