826b980b081117329bb469b4e1828a78984860da
SGLover28 <lilian.hennechart.ETU@univ-lemans.fr>
2021-05-21 16:37:19 +0200

    Pas de crash

--- app/src/main/java/com/example/projet_cleder_resolution/AjoutResolution.java
+++ app/src/main/java/com/example/projet_cleder_resolution/AjoutResolution.java
@@ -1,13 +1,12 @@
 package com.example.projet_cleder_resolution;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AjoutResolution extends AppCompatActivity {
 
-    private TextView mTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
