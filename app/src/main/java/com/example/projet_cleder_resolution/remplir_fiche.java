fa16985ce6cfa1eef84cb823b14470db0423641e
apb <ant.saillour@gmail.com>
2021-05-26 11:26:55 +0200

    bouton save redirection vers fin question

--- app/src/main/java/com/example/projet_cleder_resolution/remplir_fiche.java
+++ app/src/main/java/com/example/projet_cleder_resolution/remplir_fiche.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class remplir_fiche extends AppCompatActivity {
@@ -13,8 +14,15 @@ public class remplir_fiche extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remplir_fiche);
+        Button bouton_save = findViewById(R.id.bouton_sauvegarder);
+        bouton_save.setOnClickListener(this::redirection);
     }
 
     public void setContentView(int activity_remplir_fiche) {
+    }
+
+    public void redirection(View view){
+        Intent intent = new Intent(this, FinQuestion.class);
+        startActivity(intent);
     }
 }
